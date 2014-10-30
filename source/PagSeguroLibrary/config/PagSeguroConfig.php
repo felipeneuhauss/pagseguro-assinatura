a5c6dbdbff2fe487c845fcd769d9d27317c03393
Felipe Neuhauss <felipe.neuhauss@gmail.com>
2014-10-30 16:34:46 -0200

    Arquivos para a biblioteca do pagseguro + recorrencia

---
+++ source/PagSeguroLibrary/config/PagSeguroConfig.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ ************************************************************************
+ PagSeguro Config File
+ ************************************************************************
+ */
+
+$PagSeguroConfig = array();
+
+$PagSeguroConfig['environment'] = array();
+$PagSeguroConfig['environment']['environment'] = "production";
+
+$PagSeguroConfig['credentials'] = array();
+$PagSeguroConfig['credentials']['email'] = "felipe.neuhauss@gmail.com";
+$PagSeguroConfig['credentials']['token'] = "200E09FE6C9E4965B5FD1A088650912C";
+
+$PagSeguroConfig['application'] = array();
+$PagSeguroConfig['application']['charset'] = "UTF-8"; // UTF-8, ISO-8859-1
+
+$PagSeguroConfig['log'] = array();
+$PagSeguroConfig['log']['active'] = false;
+$PagSeguroConfig['log']['fileLocation'] = "";
